e01c546db02de8d8121c0848e06bd6671ef9d108
giorgichkonidze <jorjika2324@gmail.com>
2025-01-10 20:48:14 +0400

    commit

---
+++ group 25/level 70/classwork/script.js
@@ -0,0 +1,7 @@
+let name = "გიორგი";
+let surname = "ჭყონიძე";
+let age = 13;
+
+console.log("my name is" + name, "my surname" + surname, "i am" + age)
+alert("my name is" + name, "my surname" + surname, "i am" + age)
+
---
+++ group 25/level 70/homework/script.js
@@ -0,0 +1,14 @@
+let num1 = 30;
+let num2 = 5;
+
+const sum = num1 + num2;        
+const difference = num1 - num2; 
+const product = num1 * num2;     
+const quotient = num1 / num2;    
+
+console.log("ჯამი: " + sum);
+console.log("სხვაობა: " + difference);
+console.log("ნამრავლი: " + product);
+console.log("განაყოფი: " + quotient);
+
+
