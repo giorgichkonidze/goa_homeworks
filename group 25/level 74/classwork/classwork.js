f42a178456591e5b57dfbeb29f80d9c8d7e06135
giorgichkonidze <jorjika2324@gmail.com>
2025-01-20 17:44:47 +0400

    commit

---
+++ group 25/level 74/classwork/classwork.js
@@ -0,0 +1,14 @@
+let choice = 10;
+    switch(choice) {
+      case 1:
+        console.log("americano");
+        break;
+      case 2:
+        console.log("espresso");
+        break;
+      case 3:
+        console.log("cappuccino")
+        break;
+      default:
+        console.log("unknow");
+    }
---
+++ group 25/level 74/homework/task 1/script.js
@@ -0,0 +1,30 @@
+function getDayName(dayNumber) {
+    let dayName;
+    switch(dayNumber) {
+        case 1:
+            dayName = "ორშაბათი";
+            break;
+        case 2:
+            dayName = "სამშაბათი";
+            break;
+        case 3:
+            dayName = "ოთხშაბათი";
+            break;
+        case 4:
+            dayName = "ხუთშაბათი";
+            break;
+        case 5:
+            dayName = "პარასკევი";
+            break;
+        case 6:
+            dayName = "შაბათი";
+            break;
+        case 7:
+            dayName = "კვირა";
+            break;
+        default:
+            dayName = "unknow";
+    }
+    console.log(dayName)
+}
+
