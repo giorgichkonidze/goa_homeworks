3ceec43c1137288a61e44b33446a50eb899e0320
giorgichkonidze <jorjika2324@gmail.com>
2025-01-20 17:56:10 +0400

    commit

--- group 25/level 74/homework/task 2/script.js
+++ group 25/level 74/homework/task 2/script.js
@@ -1,21 +1,20 @@
-function Weather(weatherNumber) {
-    let weatherType;
-    switch(weatherNumber) {
-        case 1:
-            weatherType = "მზიანი";
-            break;
-        case 2:
-            weatherType = "წვიმიანი";
-            break;
-        case 3:
-            weatherType = "მოღრუბლული";
-            break;
-        case 4:
-            weatherType = "ქარიანი";
-            break;
-        default:
-            weatherType = "unknow";
-    }
-    console.log(weatherNumber)
+let weatherType;
+switch(weatherNumber) {
+    case 1:
+        weatherType = "მზიანი";
+        break;
+    case 2:
+        weatherType = "წვიმიანი";
+        break;
+    case 3:
+        weatherType = "მოღრუბლული";
+        break;
+    case 4:
+        weatherType = "ქარიანი";
+        break;
+    default:
+        weatherType = "unknow";
 }
+console.log(weatherNumber)
+
 
