890d091651967fd34b6fc16431abe9dff3ece7cb
giorgichkonidze <jorjika2324@gmail.com>
2025-07-19 13:03:00 +0400

    commit

---
+++ group 25/level 121/classwork/task 01/script.js
@@ -0,0 +1,7 @@
+class dog {
+    constructor(name) {
+        this.name=name
+    }
+}
+let BuchoDog = new dog("Bucho")
+console.log(BuchoDog)
---
+++ group 25/level 121/classwork/task 02/script.js
@@ -0,0 +1,9 @@
+class dog {
+    bark() {
+        console.log('woof!')
+    }
+}
+
+let BuchoDog = new dog()
+
+BuchoDog.bark()
