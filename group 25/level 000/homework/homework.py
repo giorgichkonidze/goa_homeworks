a522f48d476f2dfba709855233d46fa1df0d78e7
giorgichkonidze <jorjika2324@gmail.com>
2025-08-11 17:14:42 +0400

    commit

--- group 25/level 000/homework/homework.py
+++ group 25/level 000/homework/homework.py
@@ -1,1 +1,14 @@
-# nothing
+from turtle import *
+
+speed(999)
+width(7)
+color("Red")
+forward(200) # წინ 200px
+left(90) # მარცხნივ 90გრადუსი
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+
+exitonclick()
