586c71725a6f640c9f6e4f240ee31e7de377f103
giorgichkonidze <jorjika2324@gmail.com>
2025-09-13 14:17:17 +0400

    commit

---
+++ group 25/level 139/homework/src/App.jsx
@@ -0,0 +1,32 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Tiger</h1>
+        <p>The tiger (Panthera tigris) is a large cat and a member of the genus Panthera native to Asia. It has a powerful, muscular body with a large head and paws, a long tail and orange fur with black, mostly vertical stripes. It is traditionally classified into nine recent subspecies, though some recognise only two subspecies, mainland Asian tigers and the island tigers of the Sunda Islands.</p>
+        <img src="./src/NVA-tiger.webp" alt="tiger"/>
+        
+        <h1>Lion</h1>
+        <p>The lion (Panthera leo) is a large cat of the genus Panthera, native to Sub-Saharan Africa and India. It has a muscular, broad-chested body; a short, rounded head; round ears; and a dark, hairy tuft at the tip of its tail. It is sexually dimorphic; adult male lions are larger than females and have a prominent mane. It is a social species, forming groups called prides. A lion's pride consists of a few adult males, related females, and cubs. Groups of female lions usually hunt together, preying mostly on medium-sized and large ungulates. The lion is an apex and keystone predator.</p>
+        <img src="./src/download.png" alt="Lion"/>
+
+        <h1>wiwila</h1>
+        <p>batoni wiwila</p>
+        <img src="./src/wiwila.jpg" alt="wiwila"/>
+
+        <h1>hey hey</h1>
+        <p>Moanas heyhey</p>
+        <img src="./src/download (1).png" alt="hey hey"/>
+
+        <h1>sea lion</h1>
+        <p>The Galápagos sea lion (Zalophus wollebaeki) is a species of sea lion that lives and breeds on the Galápagos Islands and, in smaller numbers, on Isla de la Plata (Ecuador).[2] They are the smallest sea lion species.[3]</p>
+        <img src="./src/images.png" alt="sea lion"/>
+
+        <h1>ixvniskarta</h1>
+        <p>იხვნისკარტა (ლათ. Ornithorhynchus anatinus) — ძუძუმწოვარი ცხოველი ერთგასავლიანების ქვეკლასისა. მისი სიგრძე 45 სმ აღწევს. აქვს გაბრტყელებული სხეული, მოკლე ფეხები, საცურაო აპკით გაერთიანებული თითები, გრძელი და ბრტყელი რქოვანი ნისკარტი. სხეული შემოსილია უხეში ფაცახით, რომლის ქვეშ ნაზი თივთიკია. ფერად მუქი მურაა. იკვებება ნაირგვარი ცხოველებით. გავრცელებულია ავსტრალიასა და ტასმანიაზე. კარგად ცურავს და ყვინთავს. ბინადრობს სოროში. მრავლდება წელიწადში ერთხელ, დებს 1-2 კვერცხს. ნაშიერს კვებავს რძით</p>
+        <img src="./src/ixvniskarta.jpg" alt="ixvniskarta"/>
+      </div>
+    </>
+  )
+}
+export default App
