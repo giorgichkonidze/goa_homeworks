e697786f92a6ae6fd37bbdb7c97437cfa0a16fd0
giorgichkonidze <jorjika2324@gmail.com>
2025-10-25 18:59:04 +0400

    commit

---
+++ group 25/level 148/homework/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>
+          Goal-oriented-academy
+        </h1>
+        <p>this is most popular academy!, because NIKA KESHELAVA and OTHER MENTOR are CHAD's</p>
+        
+        <p>this is mini website yet, because this is only try <br></br><h2>react vite</h2></p>
+      </div>
+    </>
+  )
+}
+
+export default App
